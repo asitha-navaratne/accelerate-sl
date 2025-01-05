bc51676ff18268b836cbc9072a65eaf39dd0b6c7
Asitha Navaratne <asithan476@gmail.com>
2025-01-06 03:27:43 +0530

    Further layout changes

--- src/constants/faqs.ts
+++ src/constants/faqs.ts
@@ -1,14 +1,14 @@
 const faqs = [
   {
-    question: "When will the session be held?",
+    question: "When will the session be held? 📆",
     answer: "After the 15th of January",
   },
   {
-    question: "What topics will be covered?",
+    question: "What topics will be covered? 💡",
     answer: "Python programming, Java programming, Web development, PowerBI",
   },
   {
-    question: "How do I register?",
+    question: "How do I register? 📝",
     answer: "Fill in the form below",
   },
 ];
--- src/views/HomePage/HomePage.tsx
+++ src/views/HomePage/HomePage.tsx
@@ -12,10 +12,13 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { ChevronRight, ClipboardType, Download } from "lucide-react";
 
 import accelerateLogo from "../../assets/logo-accelerate.png";
+import gskLogo from "../../assets/logo-gsk.png";
+import pyDataLogo from "../../assets/logo-pydata.png";
 import repoIcon from "../../assets/icon-repo.svg";
-import downloadIcon from "../../assets/icon-download.svg";
 
 import repos from "@/constants/repos";
 import faqs from "@/constants/faqs";
@@ -23,18 +26,34 @@ import faqs from "@/constants/faqs";
 const HomePage = () => {
   return (
     <div className="w-full h-full flex flex-col items-center">
-      <div className="w-full flex flex-col items-center mb-10">
-        <h1 className="inline-block text-4xl md:text-5xl uppercase italic font-extrabold tracking-tighter -rotate-3 text-purple-200 text-shadow hover:scale-125 transition-all duration-300 ease-in-out">
-          mastering
-        </h1>
-        <h1 className="inline-block text-5xl md:text-6xl uppercase italic font-extrabold tracking-tighter -rotate-3 text-yellow-500 text-shadow hover:scale-125 transition-all duration-300 ease-in-out">
-          data science
-        </h1>
+      <div className="w-full flex flex-col items-center mt-6 mb-10">
+        <h1 className="hero-text text-purple-100">mastering</h1>
+        <h1 className="hero-text text-yellow-400">data science</h1>
         <img src={accelerateLogo} className="mt-4 w-1/2 md:w-1/5" />
       </div>
+      <div>
+        <h2 className="sub-heading">🫱🏽‍🫲🏼 In partnership with:</h2>
+        <div className="flex items-center mb-10">
+          <img src={gskLogo} className="hero-logos mr-5" />
+          <img src={pyDataLogo} className="hero-logos ml-5" />
+        </div>
+      </div>
+      <div className="w-full flex flex-col items-center mt-10 mb-10">
+        <h2 className="sub-heading">
+          Have a look at the form below to register! 👀
+        </h2>
+        <Button
+          variant="outline"
+          className="bg-purple-500 border-purple-900 text-purple-50 hover:bg-purple-600 hover:text-purple-50 transition-colors duration-300"
+        >
+          <ClipboardType />
+          Visit Form
+          <ChevronRight />
+        </Button>
+      </div>
       <div className="w-[70%] sm:w-1/2">
-        <h2 className="font-bold justify-self-center text-center mb-5 hover:text-purple-600 transition-colors duration-300">
-          Browse our FAQs to answer your burning questions
+        <h2 className="sub-heading">
+          Browse our FAQs to answer your burning questions! 🙋‍♂️
         </h2>
         <Accordion type="single" collapsible className="w-full">
           {faqs.map((faq, index) => (
@@ -46,9 +65,7 @@ const HomePage = () => {
         </Accordion>
       </div>
       <div className="mt-10">
-        <h2 className="font-bold justify-self-center text-center mb-5 hover:text-purple-600 transition-colors duration-300">
-          Or go through our repositories below
-        </h2>
+        <h2 className="sub-heading">Or go through our repositories below 🧑‍💻</h2>
         <Carousel
           opts={{
             align: "start",
@@ -59,27 +76,27 @@ const HomePage = () => {
           <CarouselContent>
             {repos.map((repo) => (
               <CarouselItem key={repo.name} className="basis-1/3">
-                <Card className="w-auto h-full">
+                <Card className="w-auto h-full bg-slate-800 border-slate-500">
                   <CardContent className="p-2 h-full">
                     <div className="flex flex-col h-full">
                       <div className="flex h-1/3 items-center">
                         <img src={repoIcon} className="w-1/6 mr-3" />
-                        <h2 className="font-semibold text-blue-700">
+                        <h2 className="font-semibold text-blue-400">
                           {repo.name}
                         </h2>
                       </div>
                       <div className="mt-2 h-1/3">
-                        <p className="text-sm">{repo.description}</p>
+                        <p className="text-sm text-white">{repo.description}</p>
                         <div className="flex items-center mt-3">
                           <div
                             className={`rounded-full bg-${repo.color} w-3 h-3`}
                           ></div>
-                          <p className="ml-2">{repo.language}</p>
+                          <p className="ml-2 text-white">{repo.language}</p>
                         </div>
                       </div>
                       <div className="flex items-center h-1/3 mt-14 justify-self-end">
-                        <img src={downloadIcon} className="w-1/6 mr-2" />
-                        <a href={repo.link} className="text-sm">
+                        <Download className="w-1/6 mr-2 text-white" />
+                        <a href={repo.link} className="text-sm text-white">
                           Download .zip file
                         </a>
                       </div>
