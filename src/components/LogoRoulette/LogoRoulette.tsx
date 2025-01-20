8fd12cff577816f3469c0f6fd8856982cbe1f3d0
Asitha Navaratne <asithan476@gmail.com>
2025-01-20 11:27:52 +0530

    Minor changes to logo roulette

--- src/components/LogoRoulette/LogoRoulette.tsx
+++ src/components/LogoRoulette/LogoRoulette.tsx
@@ -1,4 +1,6 @@
-import { motion } from "motion/react";
+import { memo } from "react";
+
+import { domAnimation, LazyMotion, m } from "motion/react";
 
 import styles from "./LogoRoulette.module.css";
 
@@ -13,87 +15,89 @@ import gitLogo from "../../assets/logo-git.png";
 const LogoRoulette = () => {
   return (
     <div className={styles["logo-roulette"]}>
-      <motion.img
-        src={pythonLogo}
-        animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
-        transition={{ duration: 4, ease: "linear", repeat: Infinity }}
-        className="w-10 mx-2 absolute z-50"
-        alt="Python logo"
-      />
-      <motion.img
-        src={powerBiLogo}
-        animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
-        transition={{
-          duration: 4,
-          ease: "linear",
-          delay: 0.5,
-          repeat: Infinity,
-        }}
-        className="h-10 mx-2 absolute z-50"
-        alt="Power BI logo"
-      />
-      <motion.img
-        src={javaScriptLogo}
-        animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
-        transition={{
-          duration: 4,
-          ease: "linear",
-          delay: 1,
-          repeat: Infinity,
-        }}
-        className="w-10 mx-2 absolute z-50"
-        alt="JavaScript logo"
-      />
-      <motion.img
-        src={htmlLogo}
-        animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
-        transition={{
-          duration: 4,
-          ease: "linear",
-          delay: 1.5,
-          repeat: Infinity,
-        }}
-        className="w-10 mx-5 absolute z-50"
-        alt="HTML logo"
-      />
-      <motion.img
-        src={cssLogo}
-        animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
-        transition={{
-          duration: 4,
-          ease: "linear",
-          delay: 2,
-          repeat: Infinity,
-        }}
-        className="w-10 mx-2 absolute z-50"
-        alt="CSS logo"
-      />
-      <motion.img
-        src={githubLogo}
-        animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
-        transition={{
-          duration: 4,
-          ease: "linear",
-          delay: 2.5,
-          repeat: Infinity,
-        }}
-        className="w-10 ml-5 mx-2 absolute z-50"
-        alt="GitHub logo"
-      />
-      <motion.img
-        src={gitLogo}
-        animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
-        transition={{
-          duration: 4,
-          ease: "linear",
-          delay: 3,
-          repeat: Infinity,
-        }}
-        className="w-10 ml-5 mx-2 absolute z-50"
-        alt="Git logo"
-      />
+      <LazyMotion features={domAnimation}>
+        <m.img
+          src={pythonLogo}
+          animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
+          transition={{ duration: 4, ease: "linear", repeat: Infinity }}
+          className="w-10 mx-2 absolute z-50"
+          alt="Python logo"
+        />
+        <m.img
+          src={powerBiLogo}
+          animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
+          transition={{
+            duration: 4,
+            ease: "linear",
+            delay: 0.5,
+            repeat: Infinity,
+          }}
+          className="h-10 mx-2 absolute z-50"
+          alt="Power BI logo"
+        />
+        <m.img
+          src={javaScriptLogo}
+          animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
+          transition={{
+            duration: 4,
+            ease: "linear",
+            delay: 1,
+            repeat: Infinity,
+          }}
+          className="w-10 mx-2 absolute z-50"
+          alt="JavaScript logo"
+        />
+        <m.img
+          src={htmlLogo}
+          animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
+          transition={{
+            duration: 4,
+            ease: "linear",
+            delay: 1.5,
+            repeat: Infinity,
+          }}
+          className="w-10 mx-5 absolute z-50"
+          alt="HTML logo"
+        />
+        <m.img
+          src={cssLogo}
+          animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
+          transition={{
+            duration: 4,
+            ease: "linear",
+            delay: 2,
+            repeat: Infinity,
+          }}
+          className="w-10 mx-2 absolute z-50"
+          alt="CSS logo"
+        />
+        <m.img
+          src={githubLogo}
+          animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
+          transition={{
+            duration: 4,
+            ease: "linear",
+            delay: 2.5,
+            repeat: Infinity,
+          }}
+          className="w-10 ml-5 mx-2 absolute z-50"
+          alt="GitHub logo"
+        />
+        <m.img
+          src={gitLogo}
+          animate={{ x: [0, 100, 0, -100, 0], scale: [0, 0.5, 1, 0.5, 0] }}
+          transition={{
+            duration: 4,
+            ease: "linear",
+            delay: 3,
+            repeat: Infinity,
+          }}
+          className="w-10 ml-5 mx-2 absolute z-50"
+          alt="Git logo"
+        />
+      </LazyMotion>
     </div>
   );
 };
 
-export default LogoRoulette;
+export default memo(LogoRoulette);
