7de9dcaf9d577138b636cd01425a72bbe18345a2
Asitha Navaratne <asithan476@gmail.com>
2025-01-22 19:53:55 +0530

    Fix for scrolling issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+
 import { BrowserRouter } from "react-router";
+
+import "./index.css";
+
+import App from "./App.tsx";
+
 import StarField from "./components/StarField/StarField.tsx";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename="/accelerate-sl">
+      <ScrollToTop />
       <App />
       <StarField />
     </BrowserRouter>
